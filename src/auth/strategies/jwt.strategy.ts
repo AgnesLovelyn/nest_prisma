09fef667bc15092ba900f157bda06ac711395462
AgnesLovelyn <agnes_elson_33rpl@student.smktelkom-mlg.sch.id>
2026-04-14 09:39:19 +0700

    debug: log jwt payload

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -4,15 +4,16 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
- constructor() {
-  super({
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    ignoreExpiration: false,
-    secretOrKey: 'secretKey', // HARUS SAMA dengan JwtModule
-  });
-}
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'secretKey',
+    });
+  }
 
   async validate(payload: any) {
+    console.log('JWT payload:', payload);
     return {
       id: payload.sub,
       nis: payload.nis,
