b1e62bc5d7651821945cec09e8b0910799f2c80b
AgnesLovelyn <agnes_elson_33rpl@student.smktelkom-mlg.sch.id>
2026-04-16 09:48:58 +0700

    repair user role

--- src/peminjaman-buku/peminjaman-buku.controller.ts
+++ src/peminjaman-buku/peminjaman-buku.controller.ts
@@ -4,6 +4,7 @@ import { UseGuards, Req } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
+import { UserRole } from '@prisma/client';
 import { CreatePeminjamanBukuDto } from './dto/create-PeminjamanBuku.dto';
 
 @Controller('peminjaman-buku')
@@ -11,44 +12,40 @@ export class PeminjamanBukuController {
   constructor(private readonly service: PeminjamanBukuService) {}
 
   // POST /peminjaman-buku (pakai NIS)
-@UseGuards(JwtAuthGuard, RolesGuard)
-@Roles('MEMBER', 'ADMIN')
-@Post()
-pinjamBuku(
-  @Req() req,
-  @Body() dto: CreatePeminjamanBukuDto,
-) {
-  return this.service.pinjamBuku(req.user.nis, dto.Id_book);
-}
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.MEMBER)
+  @Post()
+  pinjamBuku(@Req() req, @Body() dto: CreatePeminjamanBukuDto) {
+    return this.service.pinjamBuku(req.user.nis, dto.Id_book);
+  }
 
-@UseGuards(JwtAuthGuard, RolesGuard)
-@Roles('ADMIN', 'PETUGAS')
-@Get()
-findAll() {
-  return this.service.findAll();
-}
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN', 'PETUGAS')
+  @Get()
+  findAll() {
+    return this.service.findAll();
+  }
 
-@UseGuards(JwtAuthGuard, RolesGuard)
-@Roles('ADMIN', 'PETUGAS')
-@Put(':id')
-kembalikanBuku(@Param('id') id: string) {
-  return this.service.kembalikanBuku(Number(id));
-}
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN', 'PETUGAS')
+  @Put(':id')
+  kembalikanBuku(@Param('id') id: string) {
+    return this.service.kembalikanBuku(Number(id));
+  }
 
- @Get('date/:date')
-getByDate(@Param('date') date: string) {
-  return this.service.getByDate(date);
+  @Get('date/:date')
+  getByDate(@Param('date') date: string) {
+    return this.service.getByDate(date);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Get('status/:id')
+  cekStatus(@Param('id') id: string) {
+    return this.service.cekStatusBuku(Number(id));
+  }
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN', 'PETUGAS')
+  @Get(':id')
+  getById(@Param('id') id: string) {
+    return this.service.getById(Number(id));
+  }
 }
-@UseGuards(JwtAuthGuard)
-@Get('status/:id')
-cekStatus(@Param('id') id: string) {
-  return this.service.cekStatusBuku(Number(id));
-}
-@UseGuards(JwtAuthGuard, RolesGuard)
-@Roles('ADMIN', 'PETUGAS')
-@Get(':id')
-getById(@Param('id') id: string) {
-  return this.service.getById(Number(id));
-}
-}
- 
