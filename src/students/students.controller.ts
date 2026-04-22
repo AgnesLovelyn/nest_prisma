14921e139747031e6b94f770be833bb07ba1bf38
AgnesLovelyn <agnes_elson_33rpl@student.smktelkom-mlg.sch.id>
2026-04-22 12:06:26 +0700

    tambah role di student

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,57 +1,64 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Param,
-  Put,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete, UseGuards } from '@nestjs/common';
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { StudentsService } from './students.service';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-students.dto';
-import { UseGuards, Req } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
 
+@ApiTags('Students')
+@ApiBearerAuth()
 @Controller('students')
 export class StudentsController {
   constructor(private readonly studentsService: StudentsService) {}
 
+  // ADMIN only
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   @Post()
   create(@Body() dto: CreateStudentDto) {
     return this.studentsService.create(dto);
   }
 
+  // ADMIN & PETUGAS
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN', 'PETUGAS')
   @Get()
   findAll() {
     return this.studentsService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN', 'PETUGAS')
   @Get('nis/:nis')
   findByNis(@Param('nis') nis: string) {
     return this.studentsService.findByNis(nis);
   }
 
- @Get('name/:name')
-findByName(@Param('name') name: string) {
-  return this.studentsService.findByName(name);
-}
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN', 'PETUGAS')
+  @Get('name/:name')
+  findByName(@Param('name') name: string) {
+    return this.studentsService.findByName(name);
+  }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN', 'PETUGAS')
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.studentsService.findOne(+id);
   }
 
-
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   @Put(':id')
-  update(
-    @Param('id') id: string, 
-    @Body() dto: UpdateStudentDto, ){
+  update(@Param('id') id: string, @Body() dto: UpdateStudentDto) {
     return this.studentsService.update(+id, dto);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.studentsService.remove(+id);
